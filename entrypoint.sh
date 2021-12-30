824dc47b93a4a68159dc08cc48548c4f7744b31a
hellobourne123 <96861248+hellobourne123@users.noreply.github.com>
2021-12-31 00:15:24 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #author https://github.com/developeranaz (don't delete this)
 rclone version
-mkdir /.config
-mkdir /.config/rclone/
-wget "$CONFIG_IN_URL" -O /.rclone.conf
+rclone config create 'mega' 'mega' 'user' 'gitegep940@xxyxi.com' 'pass' 'qwerty1234'
 rclone listremotes
-rclone serve http $CLOUDNAME: --addr :$PORT --vfs-read-chunk-size 128M
+rclone serve http mega: --addr :$PORT --vfs-read-chunk-size 128M
